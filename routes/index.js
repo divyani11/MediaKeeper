e084550231140400801490306ffc4c6214e5fa44
divyani11 <divyani.rajani@gmail.com>
2021-06-27 00:50:44 -0600

    app.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var dotenv = require('dotenv'); 
+const mongoClient = require('mongodb').MongoClient;
 
 //Import the module
 dotenv.config();
@@ -8,10 +9,41 @@ dotenv.config();
 //Set up default connection
 var mongoDB = process.env.MONGODB_URI
 
+//Connecting to Database
+mongoClient.connect(mongoDB,function(err,client){
+  console.log('Connected with DB')
+})
+
+//multer object creation
+var multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+  }
+})
+ 
+var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'MediaKeeper' });
+});
+
+router.get('/signin', function(req, res) {
+  res.render('signin');
+});
+
+router.get('/userhome', function(req, res) {
+  res.render('userhome');
+});
+
+/* Upload Files */
+router.post('/',upload.single('imageupload'), function(req,res)
+{
+  res.send("File upload sucessfully.");
 });
 
 module.exports = router;
