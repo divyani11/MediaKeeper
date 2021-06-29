47734ac9248b8a4d6ba56d8935006ce8e317d29e
divyani11 <divyani.rajani@gmail.com>
2021-06-29 00:40:22 -0600

    rotuing of pages updated

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,10 @@ dotenv.config();
 //Set up default connection
 var mongoDB = process.env.MONGODB_URI
 
-//Connecting to Database
-mongoClient.connect(mongoDB,function(err,client){
-  console.log('Connected with DB')
-})
 
 //multer object creation
 var multer  = require('multer')
+var upload = multer({ storage: storage })
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/uploads/')
@@ -24,20 +21,32 @@ var storage = multer.diskStorage({
         cb(null, file.originalname)
   }
 })
- 
-var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MediaKeeper' });
 });
 
+/* GET SignIn page. */
 router.get('/signin', function(req, res) {
   res.render('signin');
 });
 
+/* GET UserHome page. */
 router.get('/userhome', function(req, res) {
   res.render('userhome');
+
+  //Connecting to Database
+  mongoClient.connect(mongoDB,function(err,client){
+  const myDataBase = client.db('MediaKeeperDB');
+  const myCollection = myDataBase.collection('UserDetails');
+
+  //Get informaion from collection
+  myCollection.find({}).toArray(function(error,documents){
+    console.log(documents);
+    client.close();
+  })
+})
 });
 
 /* Upload Files */
