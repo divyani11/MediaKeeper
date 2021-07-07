53ac9bd2bfdc350d49dc4e74b56e1d361e7f141b
divyani11 <divyani.rajani@gmail.com>
2021-07-07 01:08:31 -0600

    login pages formatted

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ if (process.env.NODE_ENV !== 'production'){
 var express = require('express');
 var router = express.Router();
 var dotenv = require('dotenv'); 
-//const mongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
-//const bcrypt = require('bcrypt')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const flash = require('express-flash')
@@ -18,14 +16,6 @@ var assert = require('assert')
 //User Model
 const User = require ('../models/User')
 require('./passport-config')(passport);
-// const initializePassport = require('./passport-config')
-// initializePassport(
-//   passport, 
-//   email => users.find( user => user.email === email),
-//   id => users.find( user => user.id === id)
-// )
-
-// const users = []
 
 //Import the module
 dotenv.config();
@@ -45,25 +35,12 @@ router.use(session({
   resave: true,
   saveUninitialized: true
 
-  // resave: false,
-  // saveUninitialized: false
 }))
 //Using Passport for Registration
 router.use(passport.initialize())
 router.use(passport.session())
 router.use(methodOverride('_method'))
 
-//multer object creation
-var multer  = require('multer');
-var upload = multer({ storage: storage })
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-  }
-})
 
 /* GET home page. */
 router.get('/',  function(req, res, next) {
@@ -80,83 +57,14 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+
+
 /* GET UserProfile page. */
-router.get('/userprofile', function(req, res, next) {
-  res.render('userprofile');
+router.get('/userprofile', checkAuthenticated,function(req, res) {
+  res.render('userprofile', { username: req.user.username});
+
 });
 
-// /* POST Login page. */
-// router.post('/login', passport.authenticate('local', {
-//   successRedirect: '/userhome',
-//   failureRedirect: '/login',
-//   failureFlash: true
-// }))
-
-/* GET UserHome page. */
-router.get('/userhome', checkAuthenticated,function(req, res) {
-  res.render('userhome', { username: req.user.username});
-
-  //Connecting to Database
-//   mongoClient.connect(mongoDB,{useNewUrlParser:true, useUnifiedTopology: true},function(err,client){
-//   const myDataBase = client.db('MediaKeeperDB');
-//   const myCollection = myDataBase.collection('UserDetails');
-
-//   //Get informaion from collection
-//   myCollection.find({}).toArray(function(error,documents){
-//     console.log(documents);
-//     client.close();
-//   })
-// })
-});
-
-//POST Registration
-// router.post('/register', async (req,res) => {
-
-//   try {
-//       const hashedPassword = await bcrypt.hash(req.body.password,10)
-      // users.push({
-      //   id: Date.now().toString(),
-      //   username: req.body.username,
-      //   email: req.body.email,
-      //   password: hashedPassword
-      // })
-      
-      // res.redirect('/login')
-
-      // var item = {
-      //   id: Date.now().toString(),
-      //   username: req.body.username,
-      //   email: req.body.email,
-      //   password: hashedPassword
-      // }
-      // mongoClient.connect(mongoDB,{useNewUrlParser:true, useUnifiedTopology: true},function(err,client){
-      //   const myDataBase = client.db('MediaKeeperDB');
-      //   console.log('step 1')
-      //   const myCollection = myDataBase.collection('UserDetails');
-      //   console.log('step 2')
-      //   myCollection.insertOne(item,function(err, result){
-      //     console.log(result);
-      //     assert.strictEqual(null, err);
-      //     console.log('Item Inserted')
-          
-      //     client.close();
-      //   })
-      //   console.log('step 3')
-      // mongoClient.connect(mongoDB,function(err, client){
-      //   assert.equal(null, err);
-      //   client.db.collection('UserDetails').insertOne(item,function(err, result){
-      //     assert.equal(null, error);
-      //     console.log('Item Inserted')
-      //     db.close();
-      //   })
-//       })
-//       res.redirect('/login')
-//   } catch{
-//     alert("Opps! Something went wrong.");
-//     res.redirect('/register')
-//   }
-//   console.log(users)
-// })
 
 //Register handle Y
 router.post('/register', (req,res)=>{
@@ -225,16 +133,12 @@ router.post('/register', (req,res)=>{
 // login handle Y
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
-      successRedirect: '/userhome',
+      successRedirect: '/userprofile',
       failureRedirect: '/login',
       failureFlash: true
     })(req, res, next);
 });
 
-// router.delete('/logout',(req,res)=>{
-//   req.logout()
-//   res.redirect('/login')
-// })
 
 //logout handle Y
 router.get('/logout',(req,res)=>{
@@ -252,15 +156,9 @@ function checkAuthenticated(req, res, next){
 
 function checkNotAuthenticated(req, res, next){
   if (req.isAuthenticated()){
-   return res.redirect('/userhome')
+   return res.redirect('/userprofile')
   }
   
 }
-
-/* Upload Files */
-router.post('/',upload.single('imageupload'), function(req,res)
-{
-  res.send("File upload sucessfully.");
-});
 
 module.exports = router;
